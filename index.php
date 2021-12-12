242f38701edb410d26534ca211eed36f5e187567
amimul21 <amimulit21@gmail.com>
2021-12-12 15:43:29 +0600

    try with axaj

---
+++ index.php
@@ -0,0 +1,89 @@
+
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title> todo management with php </title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<h3 class="title">This is todo management </h3>
+
+	<div class="input_todo">
+		<form action="insert_with_ajax.php" method="POST" name="insert_form">
+			<legend id="todo_legend">Insert Data
+			<input type="text" name="todo_title" mode="<?php echo $is_update?>" id="todo_title" placeholder="insert todo.." value="<?php echo isset($update_item_name)?>" autofocus autocomplete="off"/>
+
+			<input type="hidden" name="type" id="" value="<?php echo $is_update?>">
+			
+			<input type="hidden" name="update_id" id="" value="<?php echo $update_item_id ?>">
+
+			<button type="button" onclick="sumbit_data(insert_form.todo_title.value, insert_form.type.value, insert_form.update_id.value)">insert</button>
+			</form>
+		</form>
+	</div>
+	
+	
+	
+	
+	
+	
+	<div> 
+		<div id="result">
+			<ul class="ul" >
+			<?php
+			if (isset($_SESSION["todo"])) {
+				foreach ($_SESSION["todo"] as $key=>$items) {?>
+				<div class="data" style="background-color:powderblue;padding:5px;">
+					data
+				</div>
+				<li class="result_list"> 
+					<div class="items"><span class="index" style="margin-right:5px;"><?php echo $key <10 ? "0".$key : $key?></span>
+						<span ><?php echo $items?></span>
+					</div>
+					<div style="display:flex; align-items:center;;">
+					
+						<form action="insert_with_ajax.php" method="POST" style="margin:0px 2px">
+							<input type="hidden" name="delete_id" id="delete_id" value='<?php echo $key;?>' />
+							<button type="submit">delet</button>
+						</form>
+						
+						<form action="insert_with_ajax.php" method="POST" style="margin:0px 2px ">
+							<input type="hidden" name="update_name" id="update" value="<?php echo $items?>" />
+							<input type="hidden" name="update_id" value="<?php echo $key?>" />
+							<input type="hidden" name="type" value="update">
+							<button> update </button>
+							
+							<br>
+							
+						
+						</form>
+						
+					</div>
+
+				</li>
+				
+			<?php }}?>
+
+			</ul>
+		</div>
+		<p id="response_text">response text</P>
+	</div>
+	
+
+	<script>
+		function sumbit_data (todo_title, type, update_id) {
+			document.getElementById("todo_title").innerHTML = "";
+			var XHR = new XMLHttpRequest();
+			XHR.onreadystatechange = function () {
+				if (this.readyState == 4 && this.status == 200) {
+					document.getElementById('response_text').innerHTML = this.responseText;
+					};
+			};
+			XHR.open ('POST', 'insert_with_ajax.php?name='+todo_title+'&type='+type, true);
+			XHR.send();
+		}
+	</script>
+</body>
+</html>
--- todo_list.php
+++ todo_list.php
@@ -1,4 +1,5 @@
 <?php 
+
 session_start();
 //session_unset();
 
@@ -11,15 +12,23 @@ print_r($_POST);
 echo "</pre>";
 
 $is_update = "insert";
-$update_item_name = '';
+$update_item_name = "";
 $update_item_id = '';
 
+// $main = array ();
+
+
+// $post_array = array('todo_title' => "my name", 'date' => date("d" ."/". "M" ."/". "Y"));
+// array_push($main, $post_array);
+// print_r($main[0]["todo_title"]);
+
 //insert data
-if((isset($_POST["todo_title"]) && $_POST["type"] == "insert") && $_POST["todo_title"] != " ") {
+if((isset($_POST["todo_title"]) && $_POST["type"] == "insert") && $_POST["todo_title"] != "") {
 	if (!$_SESSION["todo"]) {
 		$_SESSION["todo"] = [];
 		array_push($_SESSION["todo"], $_POST["todo_title"]);
 	}else{
+		
 		array_push($_SESSION["todo"], $_POST["todo_title"]);
 	}
 };
@@ -40,7 +49,7 @@ if ( (isset($_POST["type"]) && $_POST["type"] == "update" && isset($_POST["todo_
 if ((isset($_POST["update_name"]) && isset($_POST["update_id"])) && $_POST["update_name"] != "") {
 	// $_SESSION[$_POST["update_id"]] = $_POST["update_name"]; 
 	$is_update = "update";
-	$update_item_name = $_POST["update_name"];
+    $update_item_name = $_POST["update_name"];
 	$update_item_id = $_POST["update_id"];
 	
 }
@@ -52,7 +61,11 @@ echo "<pre>";
 	print_r($_SESSION);
 
 echo "</pre>";
+
+
 ?>
+
+
 
 
 <!DOCTYPE HTML>
@@ -68,13 +81,13 @@ echo "</pre>";
 	<div class="input_todo">
 		<form action="todo_list.php" method="POST">
 			<legend id="todo_legend">Insert Data
-			<input type="text" name="todo_title" mode="<?php echo $is_update?>" id="todo_title" placeholder="insert todo.." value="<?php echo $update_item_name?>" autofocus/>
+			<input type="text" name="todo_title" mode="<?php echo $is_update?>" id="todo_title" placeholder="insert todo.." value="<?php if (isset($update_item_name)){echo $update_item_name;}?>" autofocus/>
 
 			<input type="hidden" name="type" id="" value="<?php echo $is_update?>">
-
+			
 			<input type="hidden" name="update_id" id="" value="<?php echo $update_item_id ?>">
 
-				<button type="submit">insert</button>
+			<button type="submit">insert</button>
 			</form>
 		</form>
 	</div>
@@ -90,6 +103,9 @@ echo "</pre>";
 			<?php
 			if (isset($_SESSION["todo"])) {
 				foreach ($_SESSION["todo"] as $key=>$items) {?>
+				<div class="data" style="background-color:powderblue;padding:5px;">
+					data
+				</div>
 				<li class="result_list"> 
 					<div class="items"><span class="index" style="margin-right:5px;"><?php echo $key <10 ? "0".$key : $key?></span>
 						<span ><?php echo $items?></span>
@@ -104,6 +120,7 @@ echo "</pre>";
 						<form action="todo_list.php" method="POST" style="margin:0px 2px ">
 							<input type="hidden" name="update_name" id="update" value="<?php echo $items?>" />
 							<input type="hidden" name="update_id" value="<?php echo $key?>" />
+							<input type="hidden" name="type" value="update">
 							<button> update </button>
 							
 							<br>
@@ -114,6 +131,7 @@ echo "</pre>";
 					</div>
 
 				</li>
+				
 			<?php }}?>
 
 			</ul>
